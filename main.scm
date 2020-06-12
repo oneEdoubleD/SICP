b77edcb3a597cb9aeb10c0925f9865d1d04aadbb
Edd Haigh <ohmyedd@gmail.com>
2020-06-12 17:08:18 +0000

    Added Exercise 1.3

--- main.scm
+++ main.scm
@@ -6,3 +6,17 @@
        (- 6 2) 
        (- 2 7)))
 
+;;; Exercise 1.3
+(define (sq x)
+        (* x x))
+
+(define (sumSq x y)
+        (+ (sq x) 
+           (sq y)))
+
+(define (sumSqLargest x y z)
+  (cond ((and (< x y) (< x z) (sumSq y z)))
+        ((and (< y x) (< y z) (sumSq x z)))
+        ((and (< z x) (< z y) (sumSq x y)))))
+
+(sumSqLargest 2 3 4) ; ans -> 25
